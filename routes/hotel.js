316c60c379aca7948af0d917fc3323259e646544
Meli Uribe <lissauribe21@gmail.com>
2022-11-12 00:52:27 -0500

    MT-B2-30

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,10 +1,11 @@
 let router = require('express').Router()
 
-let { create, read, readOne, update } = require('../controllers/hotel')
+let { create, read, readOne, update, destroyOne } = require('../controllers/hotel')
 
 router.post('/', create)
 router.get('/', read)
 router.get('/:id', readOne)
 router.patch('/:id', update)
+router.delete('/:id', destroyOne)
 
 module.exports = router;
