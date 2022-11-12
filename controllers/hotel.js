28709ca20ad190c05c44bf8a665202f6b0414c3d
Meli Uribe <lissauribe21@gmail.com>
2022-11-11 22:59:45 -0500

    MT-02-28

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -79,6 +79,37 @@ const controller = {
             });
         }
     },
+
+    update: async (req, res) => {
+        let { id } = req.params;
+
+        try {
+            let oneHotel = await Hotel.findOneAndUpdate({ _id: id }, req.body, { new: true });
+            if (oneHotel) {
+                res.status(200).json({
+                    success: true,
+                    message: 'Hotel updated succesfully',
+                    data: oneHotel,
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'Hotel not found',
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 }
 
-module.exports = controller
+
+module.exports = controller;
+
+
+
+
+
--- routes/hotel.js
+++ routes/hotel.js
@@ -1,13 +1,10 @@
 let router = require('express').Router()
 
-let { create, read, readOne } = require('../controllers/hotel')
+let { create, read, readOne, update } = require('../controllers/hotel')
 
 router.post('/', create)
 router.get('/', read)
 router.get('/:id', readOne)
-router.get('/shows?hotelId=id', read)
-
-
-
+router.patch('/:id', update)
 
 module.exports = router;
