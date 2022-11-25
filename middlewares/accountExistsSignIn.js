cfb1e13ad8f12c550c593052d3806abb0e318abe
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-11-25 17:36:49 -0300

    FIX-MT-02-46

--- middlewares/accountExistsSignIn.js
+++ middlewares/accountExistsSignIn.js
@@ -4,7 +4,16 @@ const { invalidCredentialsResponse } = require("../config/responses");
 async function accountExistsSignIn(req, res, next) {
     const user = await User.findOne({email: req.body.email})
     if (user) {
-        req.user = user
+        req.user = {
+            id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            photo: user.photo,
+            email: user.email,
+            password: user.password,
+            role: user.role,
+            verified: user.verified,
+        }
         return next()
     }
     return invalidCredentialsResponse(req,res)
