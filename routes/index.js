c9e1a92dc7333db87b7e8f2c261949909b8066ef
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-11-30 05:32:45 -0300

    MT-01-55

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ let city = require('./city');
 let itinerary = require('./itinerary');
 let hotel = require ('./hotel')
 let show = require ('./show')
+let reaction = require('./reaction');
 
 
 router.use('/auth', user);
@@ -12,5 +13,6 @@ router.use('/cities', city);
 router.use('/itineraries', itinerary);
 router.use('/hotels', hotel);
 router.use('/shows', show);
+router.use('/reactions', reaction);
 
 module.exports = router;
