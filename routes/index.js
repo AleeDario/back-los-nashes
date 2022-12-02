7a030601f684586a44d2aee286c2676c0fc5b554
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-12-02 09:31:26 -0300

    TareasTerminadas

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ let itinerary = require('./itinerary');
 let hotel = require ('./hotel')
 let show = require ('./show')
 let reaction = require('./reaction');
+let comment = require('./comment');
 
 
 router.use('/auth', user);
@@ -14,5 +15,6 @@ router.use('/itineraries', itinerary);
 router.use('/hotels', hotel);
 router.use('/shows', show);
 router.use('/reactions', reaction);
+router.use('/comments', comment);
 
 module.exports = router;
